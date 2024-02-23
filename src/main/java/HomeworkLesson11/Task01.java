a953f8c49d5bb57d979252c4768ff1ec9abe3a67
ViktorRomanenko05 <160174730+ViktorRomanenko05@users.noreply.github.com>
2024-02-23 23:20:30 +0100

    Homework 11

--- src/main/java/HomeworkLesson11/Task01.java
+++ src/main/java/HomeworkLesson11/Task01.java
@@ -4,34 +4,13 @@ import java.util.Scanner;
 
 public class Task01 {
 
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-
-        String toyotaHistory = getToyota();
-        String toyota9000 = ToyotaPer1();
-        String toyota0010 = ToyotaPer2();
-        String toyota1024 = ToyotaPer3();
-
-        String volksHistory = getVolks();
-        String volks9000 = VolksPer1();
-        String volks0010 = VolksPer2();
-        String volks1024 = VolksPer3();
-
-        String fordHistory = getFord();
-        String ford9000 = FordPer1();
-        String ford0010 = FordPer2();
-        String ford1024 = FordPer3();
-
-        String kiaHistory = getKia();
-        String kia9000 = KiaPer1();
-        String kia0010 = KiaPer2();
-        String kia1024 = KiaPer3();
 
         String choice = "1";
 
-        Scanner scanner = new Scanner(System.in);
-
         do {
-
             boolean validInput = false;
             int range = 0;
             int year = 0;
@@ -49,7 +28,7 @@ public class Task01 {
 
             System.out.println("Пожалуйста, введите интересующую марку автомобиля:");
 
-            manufacturer = scanner.next().toLowerCase();
+            manufacturer = scanner.next().toLowerCase().trim();
 
             while (!validInput) {
 
@@ -62,9 +41,7 @@ public class Task01 {
                             "Введите год корректно");
                     scanner.next();
                 }
-
             }
-
 
             country = switch (manufacturer) {
                 case "toyota" -> "Япония";
@@ -87,102 +64,75 @@ public class Task01 {
                         "А пока можете ознакомиться с общей историей бренда.");
             }
 
-
             if (manufacturer.equals("toyota")) {
 
                 System.out.println("\nTOYOTA\n");
                 System.out.println("Страна производства: " + country + "\n");
-                System.out.println(toyotaHistory + "\n");
+                getToyota();
 
                 switch (range) {
-                    case 1:
-                        System.out.println(toyota9000);
-                        break;
-                    case 2:
-                        System.out.println(toyota0010);
-                        break;
-                    case 3:
-                        System.out.println(toyota1024);
-                        break;
+                    case 1 -> toyotaPeriod1();
+                    case 2 -> toyotaPeriod2();
+                    case 3 -> toyotaPeriod3();
                 }
             } else if (manufacturer.equals("volkswagen")) {
 
                 System.out.println("\nVOLKSWAGEN\n");
                 System.out.println("Страна производства: " + country + "\n");
-                System.out.println(volksHistory + "\n");
+                getVolks();
 
                 switch (range) {
-                    case 1:
-                        System.out.println(volks9000);
-                        break;
-                    case 2:
-                        System.out.println(volks0010);
-                        break;
-                    case 3:
-                        System.out.println(volks1024);
-                        break;
+                    case 1 -> volksPeriod1();
+                    case 2 -> volksPeriod2();
+                    case 3 -> volksPeriod3();
                 }
             } else if (manufacturer.equals("ford")) {
 
                 System.out.println("\nFORD\n");
                 System.out.println("Страна производства: " + country + "\n");
-                System.out.println(fordHistory + "\n");
+                getFord();
 
                 switch (range) {
-                    case 1:
-                        System.out.println(ford9000);
-                        break;
-                    case 2:
-                        System.out.println(ford0010);
-                        break;
-                    case 3:
-                        System.out.println(ford1024);
-                        break;
+                    case 1 -> fordPeriod1();
+                    case 2 -> fordPeriod2();
+                    case 3 -> fordPeriod3();
                 }
             } else if (manufacturer.equals("kia")) {
 
                 System.out.println("\nKIA\n");
                 System.out.println("Страна производства: " + country + "\n");
-                System.out.println(kiaHistory + "\n");
+                getKia();
 
                 switch (range) {
-                    case 1:
-                        System.out.println(kia9000);
-                        break;
-                    case 2:
-                        System.out.println(kia0010);
-                        break;
-                    case 3:
-                        System.out.println(kia1024);
-                        break;
+                    case 1 -> kiaPeriod1();
+                    case 2 -> kiaPeriod2();
+                    case 3 -> kiaPeriod3();
                 }
             } else {
-                System.out.println("\nВы не верно ввели марку автомобиля\n");
+                System.out.println("\nВведенная марка автомобиля не найдена\n");
             }
 
             System.out.println("\nДля продолжения работы введите 1,\n" +
                     "для завершения - любое другое значение");
 
-            choice = scanner.next();
-
+            choice = scanner.next().trim();
 
         } while (choice.equals("1"));
 
         scanner.close();
     }
 
-    public static String getToyota(){
-        String toyotaHist = "Компания Toyota, основанная в 1937 году Киичиро Тойода,\n" +
+    public static void getToyota(){
+        System.out.println( "Компания Toyota, основанная в 1937 году Киичиро Тойода,\n" +
                 "изначально была отделением Toyoda Automatic Loom Works, занимающимся\n" +
                 "производством ткацких станков, под руководством его отца, Сакичи Тойоды.\n" +
                 "Переход к производству автомобилей был мотивирован желанием Киичиро\n" +
                 "диверсифицировать бизнес семьи и способствовать индустриализации Японии.\n" +
-                "\n";
-        return toyotaHist;
+                "\n");
     }
 
-    public static String ToyotaPer1(){
-        String toyotaP1 = "1990-е: Глобализация и инновации\n" +
+    public static void toyotaPeriod1(){
+        System.out.println("1990-е: Глобализация и инновации\n" +
                 "Расширение в Северной Америке: Toyota укрепила своё присутствие в Северной Америке,\n" +
                 "открыв новые заводы и расширяя производство в регионе. Это помогло компании укрепить\n" +
                 "свои позиции на крупнейшем автомобильном рынке мира.\n" +
@@ -192,129 +142,108 @@ public class Task01 {
                 "на разработке экологически чистых автомобилей.\n" +
                 "В 1997 году был представлен Toyota Prius — первый в мире массово производимый\n" +
                 "гибридный автомобиль, который стал символом направления компании на создание более\n" +
-                "экологически чистых транспортных средств.";
-
-        return toyotaP1;
+                "экологически чистых транспортных средств.");
     }
 
-    public static String ToyotaPer2() {
-        String toyotaP2 = "В период с 2000 по 2010 год Toyota укрепила свои позиции\n" +
+    public static void toyotaPeriod2() {
+        System.out.println("В период с 2000 по 2010 год Toyota укрепила свои позиции\n" +
                 "как один из ведущих мировых автопроизводителей, в значительной степени благодаря успеху\n" +
                 "своих гибридных моделей, включая популярный Toyota Prius.\n" +
                 "Компания также столкнулась с вызовами, включая масштабные отзывы автомобилей из-за проблем\n" +
-                "с безопасностью.";
-
-        return  toyotaP2;
+                "с безопасностью.");
     }
 
-    public static String ToyotaPer3(){
-        String toyotaP3 = "С 2010 по 2024 год Toyota активно развивала технологии электромобилей\n" +
+    public static void toyotaPeriod3() {
+        System.out.println("С 2010 по 2024 год Toyota активно развивала технологии электромобилей\n" +
                 "и автономного вождения, укрепляя свою репутацию в области инноваций и устойчивого развития.\n" +
                 "Компания также расширила своё присутствие на глобальных рынках, несмотря на возрастающую\n" +
-                "конкуренцию и вызовы, связанные с мировыми экономическими и экологическими изменениями.";
-
-        return toyotaP3;
+                "конкуренцию и вызовы, связанные с мировыми экономическими и экологическими изменениями.");
     }
 
-    public static String getVolks(){
-        String volksHist = "Фольксваген, основанный в 1937 году, к 21-му веку превратился в одного из крупнейших\n" +
+    public static void getVolks(){
+        System.out.println("Фольксваген, основанный в 1937 году, к 21-му веку превратился в одного из крупнейших\n" +
                 "мировых автопроизводителей, активно развивая линейку электромобилей и инновационные технологии.\n" +
                 "Компания столкнулась с серьёзным испытанием в виде скандала с дизельными двигателями в 2015 году,\n" +
-                "но продолжила адаптацию к меняющимся рыночным и экологическим требованиям.";
-        return volksHist;
+                "но продолжила адаптацию к меняющимся рыночным и экологическим требованиям.\n");
     }
 
-    public static String VolksPer1(){
-        String volksP1 = "В 1990-е годы Volkswagen сосредоточилась на глобальной экспансии и диверсификации своего\n" +
+    public static void volksPeriod1(){
+        System.out.println("В 1990-е годы Volkswagen сосредоточилась на глобальной экспансии и диверсификации своего\n" +
                 "портфолио, приобретя бренды, такие как Skoda и Bentley, и укрепив своё присутствие на международных\n" +
                 "рынках. Этот период также ознаменовался усилиями по улучшению качества и инновационности продукции,\n" +
-                "что способствовало росту продаж и репутации компании.";
-        return volksP1;
+                "что способствовало росту продаж и репутации компании.");
     }
 
-    public static String VolksPer2(){
-        String volksP2 = "С 2000 по 2010 год Volkswagen активно расширял своё портфолио, запуская ключевые модели,\n" +
+    public static void volksPeriod2(){
+        System.out.println("С 2000 по 2010 год Volkswagen активно расширял своё портфолио, запуская ключевые модели,\n" +
                 "такие как Touareg и Passat CC, и укрепляя свои позиции в Китае, ставшем крупнейшим рынком компании.\n" +
                 "В это десятилетие VW также сделал значительный акцент на развитии экологически чистых технологий,\n" +
-                "включая гибридные и электрические автомобили.";
-        return volksP2;
+                "включая гибридные и электрические автомобили.");
     }
 
-    public static String VolksPer3(){
-        String volksP3 = "С 2010 по 2024 год Volkswagen активно развивал электрические и гибридные технологии,\n" +
+    public static void volksPeriod3(){
+        System.out.println("С 2010 по 2024 год Volkswagen активно развивал электрические и гибридные технологии,\n" +
                 "запуская значимые модели, такие как ID.3, и стремясь к лидерству в сегменте электромобилей. В этот\n" +
                 "период компания также стремилась восстановить свою репутацию после скандала с дизельными двигателями\n" +
-                "в 2015 году, сосредоточив внимание на устойчивом развитии и инновациях.";
-        return volksP3;
+                "в 2015 году, сосредоточив внимание на устойчивом развитии и инновациях.");
     }
 
-    public static String getFord(){
-        String fordHist = "Ford Motor Company, основанная Генри Фордом в 1903 году, революционизировала автомобильную\n" +
+    public static void getFord(){
+        System.out.println("Ford Motor Company, основанная Генри Фордом в 1903 году, революционизировала автомобильную\n" +
                 "промышленность внедрением линейного производства, делая автомобили доступными для широких масс.\n" +
                 "В последующие десятилетия компания продолжала инновационные разработки, расширяя свой глобальный\n" +
-                "охват и адаптируясь к изменениям рынка с акцентом на устойчивость и электромобили.";
-        return fordHist;
+                "охват и адаптируясь к изменениям рынка с акцентом на устойчивость и электромобили.\n");
     }
 
-    public static String FordPer1(){
-        String fordP1 = "В 1990-е годы Ford Motor Company активно расширяла свой глобальный охват, приобретая такие\n" +
+    public static void fordPeriod1(){
+        System.out.println("В 1990-е годы Ford Motor Company активно расширяла свой глобальный охват, приобретая такие\n" +
                 "компании, как Jaguar и Volvo, и укрепляя своё присутствие на международных рынках.\n" +
                 "Этот период также ознаменовался интенсивной работой над инновациями в области безопасности и\n" +
                 "эффективности топливопотребления, включая разработку и внедрение новых технологий и моделей\n" +
-                "автомобилей.";
-        return fordP1;
+                "автомобилей.");
     }
 
-    public static String FordPer2(){
-        String fordP2 = "В период с 2000 по 2010 год Ford Motor Company столкнулась с серьезными вызовами,\n" +
+    public static void fordPeriod2(){
+        System.out.println("В период с 2000 по 2010 год Ford Motor Company столкнулась с серьезными вызовами,\n" +
                 "включая экономический спад и мировой финансовый кризис, но смогла адаптироваться,\n" +
                 "сосредоточив усилия на улучшении качества, эффективности и разработке инновационных продуктов,\n" +
                 "таких как гибридные автомобили. Компания также провела реструктуризацию, сократив расходы и\n" +
                 "сфокусировавшись на своих основных брендах, что позволило ей восстановить финансовую стабильность\n" +
-                "без прямой государственной поддержки.";
-        return fordP2;
+                "без прямой государственной поддержки.");
     }
 
-    public static String FordPer3(){
-        String fordP3 = "С 2010 по 2024 год Ford ускорил свои инновационные инициативы, сделав ставку на\n" +
+    public static void fordPeriod3(){
+        System.out.println("С 2010 по 2024 год Ford ускорил свои инновационные инициативы, сделав ставку на\n" +
                 "электрификацию, цифровые технологии и автономное вождение. Компания значительно расширила\n" +
                 "свой портфель электромобилей, представив модели, такие как полностью электрический\n" +
                 "Mustang Mach-E и электрический пикап F-150 Lightning, подчеркивая свою приверженность устойчивому\n" +
-                "развитию и ответственному подходу к будущему автомобилестроения.";
-        return fordP3;
+                "развитию и ответственному подходу к будущему автомобилестроения.");
     }
 
-    public static String getKia(){
-        String kiaHist = "Kia, основанная в 1944 году в Южной Корее, первоначально производила стальные трубы и\n" +
+    public static void getKia(){
+        System.out.println("Kia, основанная в 1944 году в Южной Корее, первоначально производила стальные трубы и\n" +
                 "велосипедные детали, прежде чем переключиться на автомобилестроение в 1960-х годах. С тех пор\n" +
                 "компания превратилась в одного из крупнейших мировых автопроизводителей с широким ассортиментом\n" +
-                "моделей, включая седаны, внедорожники и минивэны.";
-        return kiaHist;
+                "моделей, включая седаны, внедорожники и минивэны.\n");
     }
 
-    public static String KiaPer1(){
-        String kiaP1 = "В 1990-е годы Kia столкнулась с финансовыми трудностями, которые кульминировали в объявлении\n" +
+    public static void kiaPeriod1(){
+        System.out.println("В 1990-е годы Kia столкнулась с финансовыми трудностями, которые кульминировали в объявлении\n" +
                 "о банкротстве в 1997 году, но вскоре после этого компания вошла в состав Hyundai Motor Group,\n" +
-                "что помогло стабилизировать её положение и расширить международное присутствие.";
-        return kiaP1;
+                "что помогло стабилизировать её положение и расширить международное присутствие.");
     }
 
-    public static String KiaPer2(){
-        String kiaP2 = "В период с 2000 по 2010 год Kia активно работала над улучшением качества и дизайна своих\n" +
+    public static void kiaPeriod2(){
+        System.out.println("В период с 2000 по 2010 год Kia активно работала над улучшением качества и дизайна своих\n" +
                 "автомобилей, что привело к значительному увеличению продаж и укреплению бренда на глобальном рынке.\n" +
                 "Особенно успешным стало внедрение нового корпоративного стиля,\n" +
-                "разработанного известным дизайнером Питером Шрайером.";
-        return kiaP2;
+                "разработанного известным дизайнером Питером Шрайером.");
     }
 
-    public static String KiaPer3(){
-        String kiaP3 = "С 2010 года Kia продолжила своё стремление к инновациям, активно внедряя передовые технологии\n" +
+    public static void kiaPeriod3(){
+        System.out.println("С 2010 года Kia продолжила своё стремление к инновациям, активно внедряя передовые технологии\n" +
                 "и электрификацию своего модельного ряда. Компания значительно расширила своё присутствие на рынке\n" +
                 "электромобилей и гибридов, представив такие модели, как Kia Niro и Kia EV6, и подтвердила свою\n" +
-                "репутацию как бренда, ориентированного на будущее автомобилестроения.";
-        return kiaP3;
+                "репутацию как бренда, ориентированного на будущее автомобилестроения.");
     }
-
-
-
 }
