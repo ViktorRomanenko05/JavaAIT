ffe1b19c1b2de28c5027ad573b9d401c6add45b9
ViktorRomanenko05 <160174730+ViktorRomanenko05@users.noreply.github.com>
2024-05-16 06:54:32 +0200

    homework_lesson_33_done

---
+++ src/test/homework33Tests/ClothesType.java
@@ -0,0 +1,14 @@
+package homework33Tests;
+
+public enum ClothesType {
+    SNICKERS("Snickers"),
+    SHORTS("Shorts"),
+    JACKET("Jacket");
+
+    String description;
+
+    ClothesType(String description) {
+        this.description = description;
+    }
+
+}
---
+++ src/test/homework33Tests/SizeForTest.java
@@ -0,0 +1,13 @@
+package homework33Tests;
+
+public enum SizeForTest {
+    XL ("xL"),
+    L ("l"),
+    M ("m");
+
+    String description;
+
+    SizeForTest(String description) {
+        this.description = description;
+    }
+}
